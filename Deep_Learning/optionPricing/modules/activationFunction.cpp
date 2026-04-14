aa569b06f0c2b669b3f83c154d358325a11684b3
Hugo <hugogpdealmeida@gmail.com>
2026-04-14 23:11:59 +0200

    Add new activation functions

--- Deep_Learning/optionPricing/modules/activationFunction.cpp
+++ Deep_Learning/optionPricing/modules/activationFunction.cpp
@@ -1,26 +1,45 @@
 #include <cmath>
+#include <algorithm>
 
-class ActivationFunction
-{   
+class ActivationFunction {   
 public:
-    static double heaviside(double x) {
-        return (x >= 0.0) ? 1.0 : 0.0;
+    static double sigmoid(double x) {
+        return (x >= 0) ? (1.0 / (1.0 + std::exp(-x))) : (std::exp(x) / (1.0 + std::exp(x)));
     }
-    static double heaviside_derivative(double x) {
-        return 1.0; 
+    static double sigmoid_derivative(double z) {
+        double a = sigmoid(z);
+        return a * (1.0 - a);
     }
 
     static double relu(double x) {
         return x > 0 ? x : 0;
     }
-    static double relu_derivative(double x) {
-        return (x > 0) ? 1.0 : 0.0;
+
+    static double relu_derivative(double z) {
+        return z > 0 ? 1.0 : 0.0;
     }
 
-    static double sigmoid(double x) {
-        return 1.0 / (1.0 + std::exp(-x));
+    static double leaky_relu(double x) {
+        return x > 0 ? x : 0.01 * x;
     }
-    static double sigmoid_derivative(double activated_value) {
-        return activated_value * (1.0 - activated_value);
+
+    static double leaky_relu_derivative(double z) {
+        return z > 0 ? 1.0 : 0.01;
+    }
+
+    static double softplus(double x) {
+        return std::log1p(std::exp(x));
+    }
+
+    static double softplus_derivative(double z) {
+        return sigmoid(z);
+    }
+    
+    static double heaviside(double x) {
+        return (x >= 0.0) ? 1.0 : 0.0;
+    }
+
+    static double heaviside_derivative(double z) {
+        return 0.0;
     } 
 };
