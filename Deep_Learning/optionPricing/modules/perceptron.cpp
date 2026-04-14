abdaeb1aa1f7e6afe3957b67ff9458debed72f37
Hugo <hugogpdealmeida@gmail.com>
2026-04-14 23:12:46 +0200

    Fix bugs

--- Deep_Learning/optionPricing/modules/perceptron.cpp
+++ Deep_Learning/optionPricing/modules/perceptron.cpp
@@ -3,31 +3,27 @@
 #include <Eigen/Dense>
 #include <functional>
 #include <string>
-
 #include "activationFunction.cpp" 
 
-class Perceptron
-{   
+class Perceptron {   
 public:
     Eigen::VectorXd weights;
-    int nb_inputs;
     double bias;
 
     std::function<double(double)> activation;
     std::function<double(double)> derivative;
 
-    Perceptron(int nb_inputs, std::string func_name) : nb_inputs(nb_inputs), weights(nb_inputs)
-    {
+    Perceptron(int nb_inputs, std::string func_name) : weights(nb_inputs) {
         std::random_device rd;
-        std::default_random_engine generator(rd());
-        std::uniform_real_distribution<double> dist(-1.0, 1.0);
+        std::default_random_engine gen(rd());
+        
+        double limit = std::sqrt(6.0 / (nb_inputs + 1));
+        std::uniform_real_distribution<double> dist(-limit, limit);
 
-        bias = dist(generator);
-
-        for (int i = 0; i < nb_inputs; i++)
-        {
-            weights(i) = dist(generator);
-        };
+        bias = dist(gen);
+        for (int i = 0; i < nb_inputs; i++) {
+            weights(i) = dist(gen);
+        }
 
         if (func_name == "sigmoid") {
             activation = ActivationFunction::sigmoid;
@@ -35,35 +31,20 @@ public:
         } else if (func_name == "relu") {
             activation = ActivationFunction::relu;
             derivative = ActivationFunction::relu_derivative;
-        } else if (func_name == "heaviside"){ 
-            activation = ActivationFunction::heaviside;
-            derivative = [](double x) { return 1.0; };
+        } else if (func_name == "leaky_relu") {
+            activation = ActivationFunction::leaky_relu;
+            derivative = ActivationFunction::leaky_relu_derivative;
+        } else if (func_name == "softplus") {
+            activation = ActivationFunction::softplus;
+            derivative = ActivationFunction::softplus_derivative;
+        } else {
+            std::cerr << "Warning: Unknown activation '" << func_name << "'. Defaulting to sigmoid." << std::endl;
+            activation = ActivationFunction::sigmoid;
+            derivative = ActivationFunction::sigmoid_derivative;
         }
-    };  
-
-    double forward(const Eigen::VectorXd& x)
-    {   
-        double z = weights.dot(x) + bias;
-
-        return activation(z);
-    };
-
-    void update_weights(const Eigen::VectorXd& x, double error, double learning_rate, double y_pred) {
-        double gradient = error * derivative(y_pred); 
-        
-        weights -= learning_rate * gradient * x;
-        bias -= learning_rate * gradient;
     }
-    
-    // Getters and setters 
-    Eigen::VectorXd get_weights()
-    {
-        return weights;
-    };
 
-    double get_bias()
-    {
-        return bias;
-    };
+    double get_z(const Eigen::VectorXd& x) {
+        return weights.dot(x) + bias;
+    }
 };
-
